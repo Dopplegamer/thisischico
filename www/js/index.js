ca0e5bc84ae26870facf604b4b77fa784da7503f
Dopplegamer <dopplegamer@gmail.com>
2014-05-26 09:37:01 -0700

    Rachelle styled the app

--- www/js/index.js
+++ www/js/index.js
@@ -1,153 +1,238 @@
-//this is the main JavaScript source for ThisIsChico
+// This is the main JavaScript source for FiggleSnitch
 var photos = [];
- 
+
 loadAllPhotos();
 hideAllViews();
- 
-displayAllPhotos();
+
+populateEditList();
+populateList();
 $('#showall').show();
 
-function displayAllPhotos(){
-	for (var i=0, i<photos.length; i++){
-		$('#showall ul").append("<li>" + photos[i]["imagepath"] + "</li>");
-	}
-}
- 
- function loadAllPhotos(){
-	if(localStorage["photos"] != null){
+function loadAllPhotos () {
+	if (localStorage["photos"] != null) {
 		photos = JSON.parse(localStorage["photos"]);
 	}
- }
- 
- function makePhotoEntry(){
+}
+
+function makePhotoEntry () {
 	//var imageData = $('#camera-photo').attr('src').replace("data:image/jpeg;base64,", "");
 	var imageData = $('#camera-photo').attr('src');
-	var imagepath = $('#image-path').html();
-	var longitude = $('#longitude').html().replace('Long: ', '');
-	var latitude = $('#latitude').html().replace('Long: ', '');
+	var imagePath = $('#image-path').html();
+	var longitude = $('#longitude').html();
+	var latitude = $('#latitude').html();
 	var description = $('#description').val();
 	
 	var photoEntry = {
-		"image" : imageData,
-		"imagepath": imagepath,
-		"longitude" : longitude,
-		"latitude" : latitude,
+		"imageData" : imageData, 
+		"imagePath": imagePath, 
+		"longitude" : longitude, 
+		"latitude" : latitude, 
 		"description" : description
 	};
 	
 	photos.push(photoEntry);
- }
- 
- function saveAllPhotos(){
+}
+
+function saveAllPhotos () {
 	localStorage.clear();
 	localStorage["photos"] = JSON.stringify(photos);
-	if (navigator.notification){
+	if (navigator.notification) {
 		navigator.notification.alert("Photo has been saved", null, "Success!", "OK");
+	} else {
+		alert("Photo has been saved");
 	}
- }
- 
- $('button.save').click(function(){
+}
+
+function saveAllPhotosAfterEdit () {
+	localStorage.clear();
+	localStorage["photos"] = JSON.stringify(photos);
+	if (navigator.notification) {
+		navigator.notification.alert("Changes have been saved", null, "Success!", "OK");
+	} else {
+		alert("Changes have been saved");
+	}
+}
+
+$('button.save').click(function () {
+	clearNewView();
 	makePhotoEntry();
 	saveAllPhotos();
-	$('#showall ul').children().remove();
-	displayAllPhotos();
- });
- 
- function hideAllViews() {
+	populateList();
+	populateEditList();
+});
+
+function hideAllViews () {
 	$('#showall').hide();
 	$('#camera').hide();
 	$('#edit').hide();
- }
- 
- $('li.viewlink').click(function(){
+}
+
+// CHANGED
+$('li.viewlink').click(function () {
 	hideAllViews();
-	if ($(this).html() == "Home"){
+	if ($(this).html() == "Home") {
+		clearFullView();
 		$('#showall').show();
-	} else if ($(this).html() == "Capture"){
+	} else if ($(this).html() == "Capture Photo") {
 		$('#camera').show();
 	} else {
-		populateEditView();
+		clearEditView();
 		$('#edit').show();
 	}
- });
- 
- function populateEditView(){
-	var photoToEdit = photos[WHICH_PHOTO_???];
-	
-	$('#camera-photo-edit').attr('src', photoToEdit['imageData']);
-	$('#image-path-dit').html(photoToEdit['imagepath']);
-	$('#longitude-edit').html(photoToEdit['longitude']);
-	$('#latitude-edit').html(photoToEdit['latitude']);
-	$('#description-edit').val(photoToEdit['description']);
- }
- 
- $('#save-edit').click(function(){
+});
+
+// NEW
+$('#edit ul').on('click', 'li', function () {
+	populateEditView($(this).attr('index'));
+});
+
+// NEW
+function populateEditList () {
+	$('#edit ul').children().remove();
+	for (var i=0; i<photos.length; i++) {
+		var newRow = '<li index="' + i + '">';
+		newRow += '<img class="thumbnail" src="file:///mnt/sdcard' + photos[i]["imagePath"] + '" />';
+		newRow += '</li>';
+		$('#edit ul').append(newRow);
+	}
+}
+
+// CHANGED
+function populateEditView (index) {
+	var photoToEdit = photos[index];
+
+	$("#camera-photo-edit").attr('src', 'file:///mnt/sdcard' + photoToEdit["imagePath"]);
+	$("#image-path-edit").html(photoToEdit["imagePath"]);
+	$("#longitude-edit").val(photoToEdit["longitude"]);
+	$("#latitude-edit").val(photoToEdit["latitude"]);
+	$("#description-edit").val(photoToEdit["description"]);
+	$("#index-edit").html(index);
+}
+
+function clearEditView () {
+	$("#camera-photo-edit").attr('src', '');
+	$("#image-path-edit").html('');
+	$("#longitude-edit").val('');
+	$("#latitude-edit").val('');
+	$("#description-edit").val('');
+	$("#index-edit").html('');	
+}
+
+function clearFullView () {
+	$("#camera-photo-view").attr('src', '');
+	$("#image-path-view").html('');
+	$("#longitude-view").html('');
+	$("#latitude-view").html('');
+	$("#description-view").html('');
+	$("#index-view").html('');	
+}
+
+function clearNewView () {
+	$("#camera-photo").attr('src', '');
+	$("#image-path").html('');
+	$("#longitude").html('');
+	$("#latitude").html('');
+	$("#description").val('');
+	$("#index").html('');	
+}
+
+// NEW
+$('#showall ul').on('click', 'li', function () {
+	populateView($(this).attr('index'));
+});
+
+// NEW
+function populateList () {
+	$('#showall ul').children().remove();
+	for (var i=0; i<photos.length; i++) {
+		var newRow = '<li index="' + i + '">';
+		newRow += '<img class="thumbnail" src="file:///mnt/sdcard' + photos[i]["imagePath"] + '" />';
+		newRow += '</li>';
+		$('#showall ul').append(newRow);
+	}
+}
+
+// CHANGED
+function populateView (index) {
+	var photoToView = photos[index];
+
+	$("#camera-photo-view").attr('src', 'file:///mnt/sdcard' + photoToView["imagePath"]);
+	$("#image-path-view").html(photoToView["imagePath"]);
+	$("#longitude-view").html(photoToView["longitude"]);
+	$("#latitude-view").html(photoToView["latitude"]);
+	$("#description-view").html(photoToView["description"]);
+	$("#index-view").html(index);
+}
+
+// CHANGED
+$("button.save-edit").click(function () {
 	var imageData = $('#camera-photo-edit').attr('src');
-	var imagepath = $('#image-path-edit').html();
-	var longitude = $('#longitude-edit').html();
-	var latitude = $('#latitude-edit').html();
+	var imagePath = $('#image-path-edit').html();
+	var longitude = $('#longitude-edit').val();
+	var latitude = $('#latitude-edit').val();
 	var description = $('#description-edit').val();
-	
-	photos[WHICH_PHOTO_???]['imageData'] = imageData;
-	photos[WHICH_PHOTO_???]['imagepath'] = imagepath;
-	photos[WHICH_PHOTO_???]['longitude'] = longitude;
-	photos[WHICH_PHOTO_???]['latitude'] = latitude;
-	photos[WHICH_PHOTO_???]['description'] = description;
-	
-	saveAllPhotos();
- });
- 
- $('button.camera-control').click(function(){
- // navigator is PhoneGap access to hardware
- 	if(navigator.camera){
- 		var options = {
- 			quality: 60,
- 			//destinationType: Camera.DestinationType.DATA_URL,
- 			destinationType: Camera.DestinationType.FILE_URI,
-			sourceType: 1,
- 			encodingType: 0
- 		};
- 		navigator.camera.getPicture(getPhoto, null, options);
-		navigator.geolocation.getCurrentPosition(getPosition, null, {enableHighAccuracy: true});
- 	}
- });
- 
- function getPhoto(imageURI) {
- 	$('#camera-photo').attr('src', imageURI);
-	window.resolveLocalFileSystemURI(imageURI, resolveOnSuccess, resolveOnError);
- }
- 
- function resolveOnError(error){
-	//do nothing for now
- }
- 
- function resolveOnSuccess(entry){
-	var now = new Date();
-	var timestamp = now.getTime();
-	var photoName = timestamp + ".jpg";
-	var photoFolder = "chico_photos";
-	
-	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
-		function(fileSystem) {
-			fileSystem.root.getDirectory(photoFolder, {create:true, exclusive: false},
-				function(directory){
-					entry.moveTo(directory, photoName, successMove, resolveOnError);
-				},
+	var index = $('#index-edit').html();
+
+	photos[index]["imageData"] = imageData;
+	photos[index]["imagePath"] = imagePath;
+	photos[index]["longitude"] = longitude;
+	photos[index]["latitude"] = latitude;
+	photos[index]["description"] = description;
+
+	saveAllPhotosAfterEdit();
+});
+
+$('button.camera-control').click(function () {
+    // navigator is PhoneGap access to hardware
+    if (navigator.camera) {
+        
+        var options = {
+            quality: 60,
+            //destinationType: Camera.DestinationType.DATA_URL,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: 1,
+            encodingType: 0
+        };
+        
+        navigator.camera.getPicture(getPhoto, null, options);
+        navigator.geolocation.getCurrentPosition(getPosition, null, {enableHighAccuracy: true});
+    }
+});
+
+function getPosition (position) {
+    var longitude = position.coords.longitude;
+    var latitude = position.coords.latitude;
+    
+    $('#longitude').html(longitude);
+    $('#latitude').html(latitude);
+}
+
+function getPhoto (imageURI) {
+    $('#camera-photo').attr('src', imageURI);
+    window.resolveLocalFileSystemURI(imageURI, resolveOnSuccess, resolveOnError);
+}
+
+function resolveOnError(error) {
+    // do nothing for now
+}
+
+function resolveOnSuccess(entry){ 
+    var now = new Date();
+    var timestamp = now.getTime();
+    var photoName = timestamp + ".jpg";
+    var photoFolder = "figgle_photos";
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
+    	function(fileSystem) {      
+    		fileSystem.root.getDirectory(photoFolder, {create:true, exclusive: false}, 
+    			function(directory) {
+					entry.moveTo(directory, photoName,  successMove, resolveOnError);
+				}, 
 				resolveOnError);
-		},
-		resolveOnError);
- }
- 
- function successMove(entry){
-	$('#image-path').html(entry.fullPath);
- }
- 
- function getPosition(position){
-	var longitude = position.coords.longitude;
-	var latitude =position.coords.latitude;
-	
-	$('#longitude').html('Long: ' + longitude); 
-	$('#latitude').html('Lat: ' + latitude); 
- }
+    	},
+    	resolveOnError);
+}
 
-
+function successMove(entry) {
+    $('#image-path').html(entry.fullPath);
+}
