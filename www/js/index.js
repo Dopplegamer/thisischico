1d626de8bd795e945d403d5e67706dc8d184fec7
Dopplegamer <dopplegamer@gmail.com>
2014-05-27 13:52:51 -0700

    moved location of some code

--- www/js/index.js
+++ www/js/index.js
@@ -184,6 +184,8 @@ $("button.save-edit").click(function () {
 
  $('button.camera-control').click(function(){
  // navigator is PhoneGap access to hardware
+		navigator.camera.getPicture(getPhoto, null, options);
+		navigator.geolocation.getCurrentPosition(getPosition, null, {enableHighAccuracy: true});
  	if(navigator.camera){
  		var options = {
  			quality: 60,
@@ -192,8 +194,7 @@ $("button.save-edit").click(function () {
 			sourceType: 1,
  			encodingType: 0
  		};
- 		navigator.camera.getPicture(getPhoto, null, options);
-		navigator.geolocation.getCurrentPosition(getPosition, null, {enableHighAccuracy: true});
+ 		
  	}
  });
 
